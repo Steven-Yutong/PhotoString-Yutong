18602f5bde8ee32238a9bfc945a09636ebb3b9b7
yutong <864256658@qq.com>
2021-12-08 20:59:54 +0800

    submit

--- Lab.py
+++ Lab.py
@@ -9,7 +9,6 @@ from flask import Flask, request,redirect,url_for
 from UseSqlite import InsertQuery, RiskQuery
 from datetime import datetime
 from PIL import Image
-from test_blueprint import test_blueprint
 from show import show_bp
 from api import api_bp
 from upload import upload_bp
@@ -23,32 +22,6 @@ app.register_blueprint(api_bp, url_prefix="/api")
 
 # 自己本地的项目绝对路径
 ch='E:/JupyterWork/PhotoString_by_ChenXintao'
-
-
-def make_html_paragraph(s):
-    if s.strip()=='':
-        return ''
-    lst=s.split(',')
-    picture_path=lst[2].strip()
-    picture_name=lst[3].strip()
-    im = Image.open(picture_path)
-    im.thumbnail((400, 300))
-    im.save(ch+'/static/figure/'+picture_name, 'jpeg')
-    result='<p>'
-    result+='<i>%s</i><br/>'%(lst[0])
-    result+='<i>%s</i><br/>'%(lst[1])
-    result+='<a href="%s"><img src="./static/figure/%s"alt="风景图"></a>'%(picture_path,picture_name)
-    return result+'</p>'
-
-
-def get_database_photos():
-    rq = RiskQuery(ch + '/static/RiskDB.db')
-    rq.instructions("SELECT * FROM photo ORDER By time desc")
-    rq.do()
-    record = '<p>My past photo</p>'
-    for r in rq.format_results().split('\n\n'):
-        record+='%s'%(make_html_paragraph(r))
-    return record+'</table>\n'
 
 
 @app.route('/', methods=['POST', 'GET'])
--- upload.py
+++ upload.py
@@ -4,6 +4,6 @@ from flask import Blueprint
 upload_bp = Blueprint("upload_bp", __name__)
 
 
-@upload_bp.route("/",methods=['GET', 'POST'])
+@upload_bp.route("/", methods=['GET', 'POST'])
 def upload():
     return "上传图片"
